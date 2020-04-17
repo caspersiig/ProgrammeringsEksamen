eac74a381e903bd45d2d1ba9f5484f2685f8204e
NatasjaDamsbjerg <natasja@damsbjerg.dk>
2020-04-17 12:34:44 +0200

    Revert "Merge branch 'Development' into MainPage"
    This reverts commit 23dee4a138e9879c1467ecaf3e6e55997a505851, reversing
    changes made to c0b623b6b23bf9de58630200aab01bf3b26f7de8.

--- js/app.js
+++ js/app.js
@@ -7,22 +7,19 @@ $(function(){
       delivery.on('delivery.connect',function(delivery){
         $("input[type=submit]").click(function(evt){
           var file = $("input[type=file]")[0].files[0];
-
-      //Her hentes dataen fra elementerne i titel, feature og description felterne på mainpage siden via document.getElementById metoden
+      
               var dataTitle = document.getElementById('titel');
               var dataFeature = document.getElementById('features');
               var dataDescription = document.getElementById('description');
-
-      //laver en datapakke, der indeholder værdierne fra inputfelterne
+      
                 fileData = {
                   title: dataTitle.value,
                   feature: dataFeature.value,
                   description: dataDescription.value
                 }
-      //Sender datapakken med navnet "fileData" til serveren
+      
                 socket.emit("fileData", fileData);
 
-                //Kalder delivery funktionen til den valgte fil
           delivery.send(file);
           evt.preventDefault();
         });
